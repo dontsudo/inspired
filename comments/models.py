2f2dd028d606db4bd10a923621ada8ea06cd28d8
sudostition <chotnt741@gmail.com>
2023-12-19 13:48:13 +0900

    feat: create comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from blog.models import BaseModel, Article
+
+
+class Comment(BaseModel):
+  body = models.TextField('body', max_length=300)
+  author = models.ForeignKey(
+      settings.AUTH_USER_MODEL,
+      verbose_name='author',
+      on_delete=models.CASCADE,
+  )
+  article = models.ForeignKey(
+      Article,
+      verbose_name='article',
+      on_delete=models.CASCADE,
+  )
+  parent_comment = models.ForeignKey(
+      'self',
+      verbose_name='parent comment',
+      blank=True,
+      null=True,
+      on_delete=models.CASCADE,
+  )
+
+  class Meta:
+    ordering = ['-id']
+    verbose_name = 'comment'
+    verbose_name_plural = 'comments'
+
+  def __str__(self):
+    return self.body
--- inspired/settings.py
+++ inspired/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'blog.apps.BlogConfig',
+    'comments.apps.CommentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
